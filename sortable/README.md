8c087307bbf6d762df02eba474b18253f318e30b
Chandan TP <chandan.tp@gmail.com>
2017-03-10 15:19:29 +0100

    Version 0.1 of Sortable challenge

---
+++ sortable/README.md
@@ -0,0 +1,17 @@
+
+### Notes
+
+1. The solution is written in Scala
+
+2. For building and running the Scala program, SBT is required.
+To install SBT, run **./installSBT.sh**
+
+Refer to http://www.scala-sbt.org/0.13/docs/Installing-sbt-on-Linux.html for information on installing SBT on Linux.
+
+3. For running the program, execute **./run.sh**
+
+4. The results file **"results.txt"** is generated in the current directory
+
+### Caveat
+* DO NOT USE OPEN JDK!
+* Please use Oracle JDK 1.8 with SBT as SBT does not play nice with Open JDK
--- sortable/src/main/scala/MatchingChallenge.scala
+++ sortable/src/main/scala/MatchingChallenge.scala
@@ -1,48 +1,51 @@
-import java.io._
-import java.nio.file._
-import net.liftweb.json._
+import java.io.{File, PrintWriter}
+import java.nio.file.Paths
+import net.liftweb.json.{compact, parse, render, Extraction, DefaultFormats}
 
 object MatchingChallenge {
 
-  implicit val formats = DefaultFormats
   case class Product(product_name: String, manufacturer: String, model: String, family: Option[String], `announced-date`:String)
   case class Listing(title: String, manufacturer: String, currency: String, price: String)
   case class ProductListings(product_name: String, listings: Array[Listing])
 
   def main(args: Array[String]) {
+    implicit val formats = DefaultFormats
+    var listings = io.Source.fromFile("listings.txt", "utf-8").getLines.map(parse(_).extract[Listing]).toList
+    val products = io.Source.fromFile("products.txt", "utf-8").getLines.map(parse(_).extract[Product]).toList
 
-    def readProducts = {
-      val lines = io.Source.fromFile("products.txt", "utf-8").getLines
-      lines.map(parse(_).extract[Product]).toList
-    }
-
-    def readListings = {
-      val lines = io.Source.fromFile("listings.txt", "utf-8").getLines
-      lines.map(parse(_).extract[Listing]).toList
-    }
-
-    val products = readProducts
-    var listings = readListings
-
-    def productListings(products: List[Product], partialListings: List[ProductListings]): List[ProductListings] = products match {
-      case Nil => partialListings
-      case product :: remainder => {
-        val plistings = product.family match {
-          case Some(family) => listings.filter(_.manufacturer.contains(product.manufacturer))
-              .filter(_.title.contains(product.family.mkString))
-              .filter(_.title.contains(product.model))
-          case None => listings.filter(_.manufacturer.contains(product.manufacturer)).filter(_.title.contains(product.model))
+    def getProductListings(products: List[Product], productList: List[ProductListings]): List[ProductListings] = products match {
+      case Nil => productList
+      case product :: remainingProducts => {
+        // Get listings which have matching product.manufacturer,
+        // product.family (if available) and product.model (ignore case)
+        val productListings = product.family match {
+          case Some(family) => listings
+            .filter(_.manufacturer.toLowerCase.contains(product.manufacturer.toLowerCase))
+            .filter(_.title.toLowerCase.contains(product.family.mkString.toLowerCase))
+            .filter(_.title.toLowerCase.contains(product.model.toLowerCase))
+          case None => listings
+            .filter(_.manufacturer.toLowerCase.contains(product.manufacturer.toLowerCase))
+            .filter(_.title.toLowerCase.contains(product.model.toLowerCase))
         }
-        listings = listings.diff(plistings)
-        productListings(remainder, ProductListings(product.product_name, plistings.toArray) :: partialListings)
+        // Remove matching product listings as a single listing can match at most one product
+        listings = listings.filterNot(productListings.toSet)
+        getProductListings(remainingProducts, ProductListings(product.product_name, productListings.toArray) :: productList)
       }
     }
 
-    val prodListings = productListings(products.filter(_.family != None), Nil) ++ productListings(products.filter(_.family == None), Nil)
-    val resultFile = Paths.get(".", "results.txt").normalize.toAbsolutePath.toString
-    val writer = new OutputStreamWriter(new FileOutputStream(resultFile), "utf-8")
-    prodListings.foreach(prodListing => writer.write(compact(render(Extraction.decompose(prodListing))) + System.getProperty("line.separator")))
-    writer.close
-    println("Generated result = %s".format(resultFile))
+    def writeToFile(productListings: List[ProductListings]): Unit = {
+      val NewLine = System.getProperty("line.separator")
+      val resultsFile = Paths.get(".", "results.txt").normalize.toAbsolutePath.toString
+      val writer = new PrintWriter(new File(resultsFile))
+      try
+        productListings.foreach(p => writer.write(compact(render(Extraction.decompose(p))) + NewLine))
+      finally
+        writer.close
+      println("Results file = %s".format(resultsFile))
+    }
+
+    val allProductListings = getProductListings(products.filter(_.family != None), Nil) ++
+        getProductListings(products.filter(_.family == None), Nil)
+    writeToFile(allProductListings)
   }
 }
